5bcd4a7212260af517e39777e38e7ce5ee02a37e
seoyeongkimkim <144990861+seoyeongkimkim@users.noreply.github.com>
2023-10-19 15:45:13 +0900

    03

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 
 int main(int argc, char *argv[]) 
 {
-	int i;
-	
-	for (i=0;i<5;i++)
-	{
-		static int temp=1;
-		printf("temp=%d\n", temp);
-		temp++;
-	}
+	int i=10;
+	printf("함수 호출전 i=%d\n",i);
+	i=inc(i);
+	printf("함수 호출후 i=%d\n",i);
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
